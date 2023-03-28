a6739261a9fba6bfd05c7189270d7f6b6fbe50fa
Jesus M <mendoza.cmps@gmail.com>
2023-03-28 15:13:45 -0700

    edited js for class

---
+++ practice.js
@@ -0,0 +1,18 @@
+// const main = document.getElementById("main");
+// console.log(main);
+
+// const listItems = document.getElementsByClassName("example-list-item");
+// console.log(listItems);
+
+// query selectors
+// const main = document.querySelector("#main");
+// console.log("Main: ", main);
+// main = document.querySelector("[data-attributeEx]");
+
+// console.log(`Ex2 Main: ${main}`);
+// // querySelectorAll - return an array
+// const listItems = document.querySelectorAll();
+
+//const listItems = document.querySelectorAll(".example-list-item");
+const listItems = document.querySelectorAll("li");
+console.log("List Items: ", listItems);
