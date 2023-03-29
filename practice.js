cab0e6653a0f903735378f8d22556702616f460d
Jesus M <mendoza.cmps@gmail.com>
2023-03-29 12:42:36 -0700

    javascript code copied

--- practice.js
+++ practice.js
@@ -14,5 +14,24 @@
 // const listItems = document.querySelectorAll();
 
 //const listItems = document.querySelectorAll(".example-list-item");
-const listItems = document.querySelectorAll("li");
-console.log("List Items: ", listItems);
+// const listItems = document.querySelectorAll("li");
+// console.log("List Items: ", listItems);
+
+prompt("This will either multiply or add 2 numbers")
+let numOne = prompt("Enter a number", "#1");
+let numTwo = prompt("Enter a another number", "#2");
+
+if (y >= 7 && y <= 1)
+{
+    getElementsByTagName(h1) = multiplyFun(numOne, numTwo)
+}
+else{
+    y = numOne + numTwo;
+    return y;
+}
+
+function multiplyFun(numOne, numTwo){
+    y = numOne * numTwo;
+    return y; 
+}
+
